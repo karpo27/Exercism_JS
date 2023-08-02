e26e5d93ca373d57b9bbfe315a62471a748740c2
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-08-02 18:09:54 -0300

    Create Solution.js

---
+++ 05 Arrays/01 Elyses Enchantments/Solution.js
@@ -0,0 +1,39 @@
+function getItem(cards, position) {
+  return cards[position];
+}
+
+function setItem(cards, position, replacementCard) {
+  cards[position] = replacementCard;
+  return cards;
+}
+
+function insertItemAtTop(cards, newCard) {
+  cards.push(newCard);
+  return cards;
+}
+
+function removeItem(cards, position) {
+  cards.splice(position, 1);
+  return cards;
+}
+
+function removeItemFromTop(cards) {
+  cards.pop();
+  return cards;
+}
+
+function insertItemAtBottom(cards, newCard) {
+  cards.unshift(newCard);
+  return cards;
+}
+
+function removeItemAtBottom(cards) {
+  cards.shift();
+  return cards;
+}
+
+function checkSizeOfStack(cards, stackSize) {
+  return stackSize == cards.length;
+}
+
+export {getItem, setItem, insertItemAtTop, removeItem, removeItemFromTop, insertItemAtBottom, removeItemAtBottom, checkSizeOfStack};
