a9210fa1e4a6ab63cd70264e421906c0a028916d
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-08-02 18:35:01 -0300

    Update Solution_2.js

--- 04 Booleans/02 Vehicle Purchase/Solution_2.js
+++ 04 Booleans/02 Vehicle Purchase/Solution_2.js
@@ -1,1 +1,16 @@
-aaa
+function needsLicense(kind) {
+  return kind == "car" || kind == "truck";
+}
+
+function chooseVehicle(option1, option2) {
+  return option1 > option2 ? option2 + " is clearly the better choice." 
+    : option1 + " is clearly the better choice.";
+}
+
+function calculateResellPrice(originalPrice, age) {
+  return age < 3 ? 0.8 * originalPrice 
+    : age > 10 ? 0.5 * originalPrice 
+    : 0.7 * originalPrice;
+}
+
+export {needsLicense, chooseVehicle, calculateResellPrice};
