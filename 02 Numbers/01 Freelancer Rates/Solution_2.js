191a582e9eb12794e7711ab4edef1374061ea900
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-08-02 14:43:56 -0300

    Create Solution_2.js

---
+++ 02 Numbers/01 Freelancer Rates/Solution_2.js
@@ -0,0 +1,21 @@
+const ratePerDay = 8;
+const daysperMonth = 22;
+
+function dayRate(ratePerHour) {
+  return ratePerHour * ratePerDay
+}
+
+function daysInBudget(budget, ratePerHour) {
+  return Math.floor(budget / dayRate(ratePerHour))
+}
+
+function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const discountMonth = Math.floor(numDays / daysperMonth);
+  const remainingMonth = numDays / daysperMonth - discountMonth;
+  
+  return Math.ceil(
+    dayRate(ratePerHour) * daysperMonth * (discountMonth * (1 - discount) + remainingMonth)
+  )
+}
+
+export {dayRate, daysInBudget, priceWithMonthlyDiscount};
